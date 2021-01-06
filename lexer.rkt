a790db5e28de41cfb0155517bbc3844938d5333c
Sepehr Feli <sepehr.feli.1377@gmail.com>
2021-01-06 20:04:16 +0330

    parser: first commit

--- lexer.rkt
+++ lexer.rkt
@@ -7,56 +7,54 @@
 
 (define basic-lexer
            (lexer
-            ("+"                            (token-PLUS))
-            ("-"                            (token-MINUS))
-            ("*"                            (token-MULT))
-            ("/"                            (token-DIV))
-            ("="                            (token-EQUALS))
-            ("=="                           (token-BEQ))
-            ("!="                           (token-NEQ))
-            (">"                            (token-GT))
-            ("<"                            (token-LT))
-            ("("                            (token-LP))
-            (")"                            (token-RP))
-            ("["                            (token-LB))
-            ("]"                            (token-RB))
-            (","                            (token-COMMA))
-            (";"                            (token-SEMIC))
-            ("return"                       (token-RETURN))
-            ("if"                           (token-IF))
-            ("then"                         (token-THEN))
-            ("else"                         (token-ELSE))
-            ("end"                          (token-END))
-            ("while"                        (token-WHILE))
-            ("do"                           (token-DO))
-            ("true"                         (token-TRUE))
-            ("false"                        (token-FALSE))
-            ("null"                         (token-NULL))
-            ("switch"                       (token-SWITCH))
-            ("case"                         (token-CASE))
+            ["+"                            (token-PLUS)]
+            ["-"                            (token-MINUS)]
+            ["*"                            (token-MULT)]
+            ["/"                            (token-DIV)]
+            ["="                            (token-EQUALS)]
+            ["=="                           (token-BEQ)]
+            ["!="                           (token-BNE)]
+            [">"                            (token-GT)]
+            ["<"                            (token-LT)]
+            ["("                            (token-LP)]
+            [")"                            (token-RP)]
+            ["["                            (token-LB)]
+            ["]"                            (token-RB)]
+            [","                            (token-COMMA)]
+            [";"                            (token-SEMIC)]
+            ["return"                       (token-RETURN)]
+            ["if"                           (token-IF)]
+            ["then"                         (token-THEN)]
+            ["else"                         (token-ELSE)]
+            ["end"                          (token-END)]
+            ["while"                        (token-WHILE)]
+            ["do"                           (token-DO)]
+            ["null"                         (token-NULL)]
+            ["switch"                       (token-SWITCH)]
+            ["case"                         (token-CASE)]
 
-            ((:: "\"" (complement (:: any-string "\"" any-string)) "\"")                                     (token-STRING   (string->symbol lexeme)))
-            ((:or (:+ (char-range #\0 #\9)) (:: (:+ (char-range #\0 #\9)) #\. (:+ (char-range #\0 #\9)))) (token-NUMBER   (string->number lexeme)))
-            ((:+ alphabetic)                                                                              (token-VARIABLE (string->symbol lexeme)))
-            ((:or "true" "false")                                                                         (token-BOOLEAN  (string->symbol lexeme)))
-            (whitespace (basic-lexer input-port))
-            ((eof) (token-EOF))))
+            [(:or "true" "false")                                                                         (token-BOOLEAN  ( if (equal? lexeme "true") #t #f))]
+            [(:+ alphabetic)                                                                              (token-VARIABLE (string->symbol lexeme))]
+            [(:or (:+ (char-range #\0 #\9)) (:: (:+ (char-range #\0 #\9)) #\. (:+ (char-range #\0 #\9)))) (token-NUMBER   (string->number lexeme))]
+            [(:: "\"" (complement (:: any-string "\"" any-string)) "\"")                                  (token-STRING   (substring lexeme 1 (- (string-length lexeme) 1)))]
+            [whitespace (basic-lexer input-port)]
+            [(eof) (token-EOF)]))
 
 (define-tokens value-tokens
   (NUMBER VARIABLE BOOLEAN STRING)
  )
 
 (define-empty-tokens empty-tokens
-  (PLUS MINUS MULT DIV EQUALS BEQ NEQ GT LT
+  (PLUS MINUS MULT DIV EQUALS BEQ BNE GT LT
    LP RP LB RB COMMA  SEMIC RETURN IF THEN ELSE
-   END WHILE DO TRUE FALSE NULL SWITCH CASE
+   END WHILE DO NULL SWITCH CASE
    EOF)
  )
 
 
 ;test
 (define lex-this (lambda (lexer input) (lambda () (lexer input))))
-(define my-lexer (lex-this basic-lexer (open-input-string "ab if 12 \"hello world\" 5")))
+(define my-lexer (lex-this basic-lexer (open-input-string "ab if 12 \"hello world\" true")))
 (my-lexer)
 (my-lexer)
 (my-lexer)
--- parser.rkt
+++ parser.rkt
@@ -12,21 +12,96 @@
             (whitespace (simple-math-lexer input-port))
             ((eof) (token-EOF))))
 
-(define-tokens a (NUM))
-(define-empty-tokens b (EOF plus))
+(define-tokens value-tokens (NUM))
+(define-empty-tokens empty-tokens (EOF plus))
 
-(define simple-math-parser
+(define basic-parser
            (parser
-            (start exp)
-            (end EOF)
-            (error void)
-            (tokens a b)
-            (grammar
-           (exp ((exp plus NUM) (list 'plusnumbers $1 $3)) ((NUM) (list 'anumber $1)))
-             )))
+             (start command)
+             (end EOF)
+             (error void)
+             (tokens value-tokens empty-tokens)
+             (grammar
+               (command
+                  [(keyword)               (list 'begin $1)]
+                  [(command SEMIC keyword) (appened $1 (list $3))]
+                )
+
+               (keyword
+                  [(if_statement)         $1]
+                  [(assignment_statement) $1]
+                  [(while_statement)      $1]
+                  [(return)               $1]
+                )
+
+               (while_statement
+                  [(WHILE exp DO command END) ]  ; TODO
+                )
+
+               (if_statement
+                  [(IF exp THEN command ELSE command END) (list 'if $2 $4 $6)]
+                )
+
+               (assignment_statement
+                  [(VARIABLE EQUALS exp) (list 'set! $2 $4)]  ;maybe wrong
+                )
+
+               (return
+                  [(RETURN exp) ]  ; TODO
+                )
+
+               (exp
+                  [(aexp) $1]
+                  [(aexp GT aexp)  (list '> $1 $3)]
+                  [(aexp LT aexp)  (list '< $1 $3)]
+                  [(aexp BEQ aexp) (list '= $1 $3)]
+                  [(aexp BNE aexp) (list 'not (list '= $1 $3))]
+                )
+
+               (aexp
+                  [(bexp)             $1]
+                  [(bexp MINUS aexp) (list '- $1 $3)]
+                  [(bexp PLUS aexp)  (list '+ $1 $3)]
+                )
+
+               (bexp
+                  [(cexp)            $1]
+                  [(cexp MULT bexp) (list '* $1 $3)]
+                  [(cexp DIV bexp)  (list '/ $1 $3)]
+                )
+
+               (cexp
+                  [(MINUS cexp)         (list '- 0 $2)]
+                  [(LP exp RP)          (list $2)]
+                  [(NUMBER)              $1]
+                  [(NULL)                $1]
+                  [(VARIABLE)            $1]
+                  [(BOOLEAN)             $1]
+                  [(STRING)              $1]
+                  [(list)                $1]
+                  [(VARIABLE listMember) $1]
+                )
+
+               (list
+                  [(listValues) (list $1)]
+                  [(LB RB)       null]
+                )
+
+               (listValues
+                  [(exp)                  (list $1)]
+                  [(exp COMMA listValues) (append (list $1) $3)]
+                )
+
+               (listMember
+                  [(LB exp RB)            (list $2)]
+                  [(LB exp RB listMember) (append (list $2) $4)]
+                )
+              )
+            )
+ )
 
 ;test
 (define lex-this (lambda (lexer input) (lambda () (lexer input))))
 (define my-lexer (lex-this simple-math-lexer (open-input-string "1+2+ 3 +   4")))
-(let ((parser-res (simple-math-parser my-lexer))) parser-res)
+(let ((parser-res (basic-parser my-lexer))) parser-res)
 
